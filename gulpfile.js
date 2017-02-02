9eebd1d7956d36d71ba93c9828bd48c614740cda
Witkod <piotrek.witkos@gmail.com>
2017-02-03 00:31:46 +0100

    baner ministrancka

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ var browserSync = require('browser-sync').create();
 // Static Server + watching scss/html files
 gulp.task('serve', function() {
     browserSync.init({
-        server: "./Stronki z Adamem 4"
+        server: "./Ministrancka"
     });
 
-    gulp.watch("Stronki z Adamem 4/*.html").on('change', browserSync.reload);
-    gulp.watch("Stronki z Adamem 4/*.css").on('change', browserSync.reload);
+    gulp.watch("Ministrancka/*.html").on('change', browserSync.reload);
+    gulp.watch("Ministrancka/*.css").on('change', browserSync.reload);
 });
 
 gulp.task('default', ['serve']);
